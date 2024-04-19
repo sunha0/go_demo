8157dd5faa1efddeb9ad1c9c3a1db6e309cb8233
sunha0 <hao.sun@t-systems.com>
2024-04-19 17:30:49 +0800

    通道channel 和 select

--- main.go
+++ main.go
@@ -1,24 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// demo1 通道误用导致的bug
+func demo1() {
+	wg := sync.WaitGroup{}
+
+	ch := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	close(ch)
+
+	wg.Add(3)
+	for j := 0; j < 3; j++ {
+		go func(j int) {
+			for {
+				task,ok := <-ch
+				if !ok{
+					break
+				}
+				// 这里假设对接收的数据执行某些操作
+				fmt.Println(j, task)
+				time.Sleep(time.Millisecond * 60)
+			}
+			wg.Done()
+		}(j)
+	}
+	wg.Wait()
+}
 
 func main() {
 
-	chan1 := make(chan int, 10)
-
-	for i := 0; i <= 10; i++ {
-
-		select {
-		case x := <-chan1:
-			fmt.Println(x)
-
-		case chan1 <- i:
-
-		default:
-			fmt.Println("啥也不干等待")
-
-		}
-
-	}
+	demo1()
 
 }
